f9a51cd0c82b5a8f90b02db648862bc1a5cba82c
BKXC46 <shushrut.deshpande@motorolasolutions.com>
2023-05-17 06:13:09 +0530

    commit of first

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -9,6 +9,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // your commits again.
         setContentView(R.layout.activity_main);
     }
 }
